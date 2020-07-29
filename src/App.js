63fdc3dae797b0a19e281724e543fa34a67733eb
Engels Prado <engelslove09@gmail.com>
2020-07-28 19:12:10 -0500

    Carpetas creadas

--- src/App.js
+++ src/App.js
@@ -7,21 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hola Mundo
-        </a>
-=======
         <h1>Engels Prado</h1>
->>>>>>> 647b49be7d3eca30f217eb7e69f2393cb4def39f
       </header>
     </div>
   );

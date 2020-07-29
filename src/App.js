082fc4e166d5791838a1f54713920c35eac979d7
Engels Prado <engelslove09@gmail.com>
2020-07-29 17:29:10 -0500

    Particle agregado

--- src/App.js
+++ src/App.js
@@ -6,11 +6,28 @@ import SobreMi from "./Page/SobreMi";
 import Header from "./Components/Header";
 import Servicios from "./Page/Servicios";
 import Portafolio from "./Page/Portafolio";
-
+import Particles from "react-particles-js";
 function App() {
   return (
     <>
       <Header />
+      <Particles
+        params={{
+          particles: {
+            line_linked: {
+              shadow: {
+                enable: true,
+                color: "#3CA9D1",
+                blur: 5,
+              },
+            },
+          },
+        }}
+        style={{
+          width: "100%",
+          backgroundImage: `url(/static/img/parallax.jpg)`,
+        }}
+      />
       <SobreMi></SobreMi>
       <Servicios></Servicios>
       <Portafolio />

551337faa3d3bf99cb3c3281e3fd99f28dffb86a
Engels Prado <engelslove09@gmail.com>
2020-07-29 19:06:24 -0500

    Bug glider

--- public/static/js/personales.js
+++ public/static/js/personales.js
@@ -1,123 +1,120 @@
 //Skills
-$(document).on('scroll', function(){
-	var skills = {
-	  ht: 90,
-	  jq: 50,
-	  rs: 40,
-	  wq: 80
+$(document).on("scroll", function () {
+  var skills = {
+    ht: 90,
+    jq: 50,
+    rs: 40,
+    wq: 80,
+  };
 
-	};
+  $.each(skills, function (key, value) {
+    var skillbar = $("." + key);
 
-	$.each(skills, function(key, value) {
-	  var skillbar = $('.' + key);
-	  
-	  skillbar.animate(
-	  {
-		width: value + "%"
-	  },
-	  3000,
-	  function() {
-		$(".speech-bubble").fadeIn();
-	  }
-	  );
-	});
-
-  })
+    skillbar.animate(
+      {
+        width: value + "%",
+      },
+      3000,
+      function () {
+        $(".speech-bubble").fadeIn();
+      }
+    );
+  });
+});
 
 //Menu
-document.addEventListener('scroll', function () {
-    const scroll = document.documentElement.scrollTop;
-    const $header = document.querySelector('#header');
+document.addEventListener("scroll", function () {
+  const scroll = document.documentElement.scrollTop;
+  const $header = document.querySelector("#header");
 
-    if(scroll > 0){
-        $header.classList.remove('nav-hide')
-        $header.classList.add('nav-show')
-    }else{
-        $header.classList.add('nav-hide')
-        $header.classList.remove('nav-show')
-    }
+  if (scroll > 0) {
+    $header.classList.remove("nav-hide");
+    $header.classList.add("nav-show");
+  } else {
+    $header.classList.add("nav-hide");
+    $header.classList.remove("nav-show");
+  }
 
-    //Back To Top
-    const $backToTop = document.querySelector('#backToTop');
+  //Back To Top
+  const $backToTop = document.querySelector("#backToTop");
 
-    if(scroll > 300){
-        $backToTop.classList.remove('back-hide')
-    }else{
-        $backToTop.classList.add('back-hide')
-    }
+  if (scroll > 300) {
+    $backToTop.classList.remove("back-hide");
+  } else {
+    $backToTop.classList.add("back-hide");
+  }
 
-    $backToTop.addEventListener('click', (e) => {
-        document.documentElement.scrollTop = 0;
-        e.preventDefault();
-    })
-
+  $backToTop.addEventListener("click", (e) => {
+    document.documentElement.scrollTop = 0;
+    e.preventDefault();
+  });
 });
 
 //Menu Responsive
 $(function () {
+  //Calculamos el ancho de la pagina
+  const ancho = $(window).width(),
+    enlaces = $("#enlaces"),
+    //enlacesLI = $('#enlaces li'),
+    btnMenu = $("#btn-menu"),
+    icono = $("#btn-menu .icono");
 
-	//Calculamos el ancho de la pagina
-	const ancho = $(window).width(),
-		enlaces = $('#enlaces'),
-		//enlacesLI = $('#enlaces li'),
-		btnMenu = $('#btn-menu'),
-		icono = $('#btn-menu .icono');
+  if (ancho <= 768) {
+    enlaces.hide();
+    //enlacesLI.show(); ADEMAS EN CSS AGREGAR AL LI DISPLAY NONE
+    icono.addClass("icon-menu");
+  }
 
-	if (ancho <= 768) {
-		enlaces.hide();
-		//enlacesLI.show(); ADEMAS EN CSS AGREGAR AL LI DISPLAY NONE
-		icono.addClass('icon-menu');
-	}
+  btnMenu.on("click", function (e) {
+    enlaces.slideToggle(500);
+    icono.toggleClass("icon-menu");
+    icono.toggleClass("icon-cancel");
+  });
 
-	btnMenu.on('click', function (e) {
-		enlaces.slideToggle(500);
-		icono.toggleClass('icon-menu');
-		icono.toggleClass('icon-cancel');
-	});
+  $(window).on("resize", function () {
+    if ($(this).width() > 768) {
+      enlaces.show();
+      icono.addClass("icon-cancel");
+      icono.removeClass("icon-menu");
+    } else {
+      enlaces.hide();
+      //enlacesLI.css('display','block');
+      icono.addClass("icon-menu");
+      icono.removeClass("icon-cancel");
+    }
+  });
 
-	$(window).on('resize', function () {
-		if ($(this).width() > 768) {
-			enlaces.show();
-			icono.addClass('icon-cancel');
-			icono.removeClass('icon-menu');
-		} else {
-			enlaces.hide();
-			//enlacesLI.css('display','block');
-			icono.addClass('icon-menu');
-			icono.removeClass('icon-cancel');
-		}
-	});
-
-	//Remuevo la clase del UL para que se vea luego de que cargue la web.
-	enlaces.removeClass('loadNone');
+  //Remuevo la clase del UL para que se vea luego de que cargue la web.
+  enlaces.removeClass("loadNone");
 });
 
-window.addEventListener('load', function(){
-	new Glider(document.querySelector('.carousel__lista'), {
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: '.carousel__indicadores',
-		arrows: {
-			prev: '.carousel__anterior',
-			next: '.carousel__siguiente'
-		},
-		responsive: [
-			{
-			  // screens greater than >= 775px
-			  breakpoint: 450,
-			  settings: {
-				// Set to `auto` and provide item width to adjust to viewport
-				slidesToShow: 1,
-				slidesToScroll: 1
-			  }
-			},{
-			  // screens greater than >= 1024px
-			  breakpoint: 800,
-			  settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			  }
-			}
-		]
-	});
+window.addEventListener("load", function () {
+  new Glider(document.querySelector(".carousel__lista"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: ".carousel__indicadores",
+    arrows: {
+      prev: ".carousel__anterior",
+      next: ".carousel__siguiente",
+    },
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 450,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        // screens greater than >= 1024px
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });

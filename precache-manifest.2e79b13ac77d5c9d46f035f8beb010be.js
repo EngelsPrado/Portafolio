7366598ceee88bd7a9534c9f6fff44cdcaf56fde
Engels Prado <engelslove09@gmail.com>
2020-07-29 20:49:35 -0500

    Updates

---
+++ precache-manifest.2e79b13ac77d5c9d46f035f8beb010be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64335f44e95587adcbc21649e272059",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "2b08d5e241dfa97f3cf2",
+    "url": "/Portafolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "b65ee953788a64985db8",
+    "url": "/Portafolio/static/css/main.d0df7563.chunk.css"
+  },
+  {
+    "revision": "2b08d5e241dfa97f3cf2",
+    "url": "/Portafolio/static/js/2.4be5e71a.chunk.js"
+  },
+  {
+    "revision": "18299489cd1d58275fa8a82a41822cd5",
+    "url": "/Portafolio/static/js/2.4be5e71a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65ee953788a64985db8",
+    "url": "/Portafolio/static/js/main.676242de.chunk.js"
+  },
+  {
+    "revision": "a5166c5545381b6e0a20",
+    "url": "/Portafolio/static/js/runtime-main.c0423103.js"
+  }
+]);
